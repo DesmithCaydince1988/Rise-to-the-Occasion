202dbd7bdd08b20f963dcf15e9fe05d7d4ab0213
DesmithCaydince1988 <137458728+DesmithCaydince1988@users.noreply.github.com>
2023-06-25 15:06:48 +0700

    Refactor asynchronous operations

---
+++ banana_apple_valley.js
@@ -0,0 +1,64 @@
+/*ES6*/
+
+const EntrepreneurshipProgram = {
+	// properties
+	mentors: [],
+	fundedBusinesses: [],
+	// methods
+	getMentors: function(){
+		// retrieve mentors from a database/API
+		// code here
+	},
+	registerMentor: function(mentor){
+		// code here
+	},
+	getFundedBusinesses: function(){
+		// retrieve funded businesses from a database/API
+		// code here
+	},
+	registerFundedBusiness: function(business){
+		// code here
+	},
+	// supports underrepresented communities 
+	provideAssistance: function(communityName){
+		// A program that supports underserved communities
+		// code here
+	},
+	mentorshipGoals: function(){
+		// Code here
+	},
+	// funding
+	applyForFunding: function(business){
+		// code here
+	},
+	grantFunding: function(funds){
+		// code here
+	},
+	mentorMatch: function(){
+		// code here
+	},
+	// these methods can be called to handle users
+	userLogin: function(username, password){
+		// code here
+	},
+	userSignup: function(firstName, lastName, email, password){
+		// code here
+	},
+	logout: function(){
+		// code here
+	},
+	// admin methods
+	adminLogin: function(username, password){
+		// code here
+	},
+	adminPanel: function(){
+		// code here
+	},
+	// contact methods
+	getContactInfo: function(){
+		// code here
+	},
+	contactUs: function(senderName, message){
+		// code here
+	},
+};
